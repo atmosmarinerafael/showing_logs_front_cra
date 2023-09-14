fd364921c5cb815838a512c9456e3050a3b17790
atmosmarinerafael <rafael@atmosmarine.com>
2023-09-14 11:23:05 -0300

    initial commit

---
+++ src/components/Header/Header.js
@@ -0,0 +1,10 @@
+import logo from "../../assets/images/logo_atm.png"
+import { HeaderTag } from "./styles"
+
+export default function Header() {
+    return(
+        <HeaderTag>
+            <img src={logo}/>
+        </HeaderTag>
+    )
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const HeaderTag = styled.header`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
